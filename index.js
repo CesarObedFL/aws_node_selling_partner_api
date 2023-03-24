9ad0b28eb4d3f22f6e9c1d4c74fb7a3e0960bf98
CesarObedFL <cesarobedfl@gmail.com>
2023-03-23 21:39:14 -0600

    [ADD] folder project structure

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const { endpoints_routes } = require('./selling_partner_endpoints/endpoints');
+
+const path = require('path');
+
+const app = express();
+
+const port = (process.env.PORT || 3000);
+
+
+// settings
+app.set('port', port);
+
+
+// middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routes
+app.get('/', (request, response) => {
+    response.send("selling-partner api");
+});
+
+app.use(endpoints_routes);
+
+
+app.listen(app.get('port'), () => {
+    console.log(`running in port ${app.get('port')}`);
+});
