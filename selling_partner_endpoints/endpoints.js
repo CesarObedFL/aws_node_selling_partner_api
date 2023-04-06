083e0c25095366027c0ae8ede4b67c4d2bd41cba
CesarObedFL <cesarobedfl@gmail.com>
2023-04-05 21:20:18 -0600

    [ADD] orders endpoint

--- selling_partner_endpoints/endpoints.js
+++ selling_partner_endpoints/endpoints.js
@@ -4,8 +4,9 @@ const express = require('express');
 const SellingPartnerAPI = require('amazon-sp-api');
 const routes = express();
 
-let selling_partner_api = new SellingPartnerAPI( // Dreamlab
-{
+const marketplaceIds = [];
+
+let selling_partner_api = new SellingPartnerAPI({
     region:'',
     refresh_token: '',
     access_token: '',
@@ -25,13 +26,53 @@ let selling_partner_api = new SellingPartnerAPI( // Dreamlab
         AWS_SELLING_PARTNER_ROLE: process.env.AWS_SELLING_PARTNER_ROLE
     },
     options:{
-      credentials_path:'~/.amzspapi/credentials',
-      auto_request_tokens:true,
-      auto_request_throttled:true,
-      version_fallback:true,
-      use_sandbox:false,
-      only_grantless_operations:false,
-      user_agent:"amazon-sp-api/<CLIENT_VERSION> (Language=Node.js/<NODE_VERSION>; Platform=<OS_PLATFORM>/<OS_RELEASE>)"
+        credentials_path:'~/.amzspapi/credentials',
+        auto_request_tokens:true,
+        auto_request_throttled:true,
+        version_fallback:true,
+        use_sandbox:false,
+        only_grantless_operations:false,
+        user_agent:"amazon-sp-api/<CLIENT_VERSION> (Language=Node.js/<NODE_VERSION>; Platform=<OS_PLATFORM>/<OS_RELEASE>)"
     }
-}
-);
+});
+
+// get orders by a time interval 
+routes.get('/spapi/orders', (request, response) => {
+    let date_from = request.query.date_from;
+    let date_to = request.query.date_to;
+    console.log(date_from); console.log(date_to);
+    var orders = [];
+    (async() => {
+        try {
+            let res = await selling_partner_api.callAPI({
+                api_path:'/orders/v0/orders',
+                method:'GET',
+                query:{
+                    MarketplaceIds: marketplaceIds,
+                    CreatedAfter: date_from.toString(),
+                    CreatedBefore: date_to.toString(),
+                    version:'beta'
+                }
+            });
+            orders.push(res.Orders);
+            while(res.NextToken) {
+                let nexttoken = res.NextToken;
+                res = await selling_partner_api.callAPI({
+                    api_path:'/orders/v0/orders',
+                    method:'GET',
+                    query:{
+                        MarketplaceIds: marketplaceIds,
+                        CreatedAfter: date_from.toString(),
+                        CreatedBefore: date_to.toString(),
+                        NextToken: nexttoken,
+                        version:'beta'
+                    }
+                });
+                orders.push(res.Orders);
+            }
+            response.json({ Orders : orders });
+        } catch(e) {
+            response.json(e);
+        }
+    })();
+});
